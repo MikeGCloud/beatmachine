4f6bf42b2793969a0f663f5f2a49aeb6230cdb8d
junkdeck <fred@mslsr.com>
2017-10-24 23:44:43 +0200

    removed all webaudio api calls and implemented Date delta time

--- script.js
+++ script.js
@@ -18,87 +18,38 @@ OHH.setAttribute("src","./snd_606/ohh.wav");
 OHH.setAttribute("preload","auto");
 
 function playDrum(sound){
-    var drum = '';
-    switch (sound) {
-        case 0:
-            //BD
-            drum = BD;
-            break;
-        case 1:
-            //SD
-            drum = SD;
-            break;
-        case 2:
-            //CHH
-            drum = CHH;
-            break;
-        case 3:
-            //OHH
-            drum = OHH;
-            break;
-    }
-    // console.log(sound);
-    drum.currentTime = 0;
-    drum.play();
-}
-
-//Sound class for playing sounds and retriggering oscillators
-class Sound{
-    constructor(context){
-        this.context = context;
-    }
-
-    init(){
-        this.osc = this.context.createOscillator();
-        this.vel = this.context.createGain();
-        this.lpf = this.context.createBiquadFilter();
-
-        this.lpf.frequency.value = 1200;
-        this.lpf.Q.value = 0;
-
-        this.osc.type = 'square';
-
-        this.osc.connect(this.lpf);
-        this.lpf.connect(this.vel);
-        // this.osc.connect(this.vel);
-        this.vel.connect(this.context.destination);
-    }
-
-    play(freq){
-        this.init();
-
-        this.osc.frequency.value = freq;
-        this.vel.gain.setValueAtTime(.2,this.context.currentTime);
-
-        // this.lpf.frequency.setValueAtTime(0,this.context.currentTime);
-        // this.lpf.frequency.linearRampToValueAtTime(1200,this.context.currentTime+0.15);
-
-        this.osc.start(this.context.currentTime);
-        this.stop();
-    }
-
-    stop(){
-        this.vel.gain.linearRampToValueAtTime(0.001,this.context.currentTime+.1);
-        // this.lpf.frequency.linearRampToValueAtTime(0.001,this.context.currentTime+.5);
-        this.osc.stop(this.context.currentTime+.15);
-    }
-}
-
-var context = new (window.AudioContext || window.webkitAudioContext)();
-
-function playSound(freq){
-    let sound = new Sound(context);
-    sound.play(freq);
+  var drum = '';
+  switch (sound) {
+    case 0:
+    //BD
+    drum = BD;
+    break;
+    case 1:
+    //SD
+    drum = SD;
+    break;
+    case 2:
+    //CHH
+    drum = CHH;
+    break;
+    case 3:
+    //OHH
+    drum = OHH;
+    break;
+  }
+  // console.log(sound);
+  drum.currentTime = 0;
+  drum.play();
 }
 
 function setStepButtonState(drum){
-    sequence[drum].forEach(function(val,index){
-        if(sequence[drum][index] === 1){
-            $('.stepbutton[data-step='+index+']').addClass('stepOn');
-        }else if(sequence[drum][index] === 0){
-            $('.stepbutton[data-step='+index+']').removeClass('stepOn');
-        }
-    });
+  sequence[drum].forEach(function(val,index){
+    if(sequence[drum][index] === 1){
+      $('.stepbutton[data-step='+index+']').addClass('stepOn');
+    }else if(sequence[drum][index] === 0){
+      $('.stepbutton[data-step='+index+']').removeClass('stepOn');
+    }
+  });
 }
 
 //refresh rate and bpm setup
@@ -106,7 +57,7 @@ function setStepButtonState(drum){
 var bpm = 102*4;      //tempo in beats per minute. multiplied by 4 for 16th notes.
 var beat = 60/bpm;     //length of one beat, in seconds
 //delta time setup
-var lastDelta = context.currentTime;
+var lastDelta = Date.now();
 var timer = 0;
 var a = 1;
 //sequencer setup
@@ -115,18 +66,11 @@ var sequenceLength = 16;
 //all sequencer steps, has to have 16 steps (16th notes)
 //0 = kick, 1 = snare, 2 = chh, 3 = ohh
 var sequence = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
-//chord note data
-var chords = [
-    [261.63,0,0,0,0,0,0,0,440.00,0,0,0,0,0,0,0],
-    [329.63,0,0,0,0,0,0,0,349.23,0,0,0,0,0,0,0],
-    [392.00,0,0,0,0,0,0,0,440.00,0,0,0,0,0,0,0],
-    [261.63/2,0,261.63/2,0,261.63/2,0,261.63/2,0,293.66/2,0,293.66/2,0,293.66/2,0,293.66/2,0]
-]
 //sequencer state
 var running = false;
 var hasLooped = false;
@@ -137,119 +81,122 @@ var currentDrum = 0;
 
 //sequencer loop
 setInterval(function(){
-    //sets deltatime, time passed since last sequence loop was called
-    var time = context.currentTime;
-    var delta = time - lastDelta
-    lastDelta = time;
-    //only add time to delta if the sequencer is running
-    if(running){
-        timer += delta;
+  //sets deltatime, time passed since last sequence loop was called
+  var time = Date.now();
+  var delta = (time - lastDelta)/1000   // divide by 1000 to convert from milliseconds to seconds
+  lastDelta = time;
+  //only add time to delta if the sequencer is running
+  if(running){
+    timer += delta;
+    console.log(delta);
+  }
+  //one beat has passed (technically the length of a 16th note because of the multiplier in setup)
+  if(timer>=beat){
+    //increase step counter, reset if all steps are played
+    if(step>=sequenceLength){
+      step = 0;
+      hasLooped = true;
     }
-    //one beat has passed (technically the length of a 16th note because of the multiplier in setup)
-    if(timer>=beat){
-        //increase step counter, reset if all steps are played
-        if(step>=sequenceLength){
-            step = 0;
-            hasLooped = true;
-        }
-        //goes through every note in the chord sequence and plays the corresponding note
-        chords.forEach(function(note){
-            // playSound(note[step]);
-            // console.log(note[step]);
-        });
-        //plays the corresponding drum if the current step is on. state is sequence, drum is step index.
-        sequence.forEach(function(seq, drum){
-            if(seq[step] === 1){
-                playDrum(drum);
-                // console.log(drum);
-            }
-        });
-        //current step light flashing
-        var prevStep = step-1;
-        if(prevStep < 0 && hasLooped){
-            prevStep = 15;
-        }
+    //plays the corresponding drum if the current step is on. state is sequence, drum is step index.
+    sequence.forEach(function(seq, drum){
+      if(seq[step] === 1){
+        playDrum(drum);
+        // console.log(drum);
+      }
+    });
+    //current step light flashing
+    var prevStep = step-1;
+    if(prevStep < 0 && hasLooped){
+      prevStep = 15;
+    }
 
-        $('.stepbutton[data-step='+(step)+']').toggleClass('stepOn');
-        $('.stepbutton[data-step='+(prevStep)+']').toggleClass('stepOn');
-        prevStep = step;
-        //if sequencer has looped around, toggle the last step
-        if(prevStep === 15 && step === 1){
-            $('.stepbutton[data-step='+(15)+']').toggleClass('stepOn');
-        }
-        //increases step
-        step++;
-        //resets timer for next sequence step
-        timer = 0;
+    $('.stepbutton[data-step='+(step)+']').toggleClass('stepOn');
+    $('.stepbutton[data-step='+(prevStep)+']').toggleClass('stepOn');
+    prevStep = step;
+    //if sequencer has looped around, toggle the last step
+    if(prevStep === 15 && step === 1){
+      $('.stepbutton[data-step='+(15)+']').toggleClass('stepOn');
     }
+    //increases step
+    step++;
+    //resets timer for next sequence step
+    timer = 0;
+  }
 },0);
 
-$('.run').on('mousedown',function(){
+$(document).ready(function(){
+
+  var bpmfield = $('.bpmfield');
+  var runButton = $('.run');
+
+
+  runButton.on('mousedown',function(){
     //starts sequencer if it's stopped
     if(!running){timer = beat;running = true;}
     //stops sequencer if it's running, also turns off step indicator
     else if(running){
-        $('.stepbutton[data-step='+(step-1)+']').toggleClass('stepOn');
-        running = false;
-        hasLooped = false;
-        step = 0;
+      $('.stepbutton[data-step='+(step-1)+']').toggleClass('stepOn');
+      running = false;
+      hasLooped = false;
+      step = 0;
     }
     $(this).toggleClass('run');
     $(this).toggleClass('stop');
     // playSound(440.00);
-});
-$('.stepbutton').on('mousedown',function(){
+  });
+  $('.stepbutton').on('mousedown',function(){
     sequence[currentDrum][$(this).data("step")] = 1 - sequence[currentDrum][$(this).data("step")];
     $(this).toggleClass('stepOn');
-});
-$('.drum').on('mousedown',function(){
+  });
+  $('.drum').on('mousedown',function(){
     $('.drum[data-drum='+currentDrum+']').removeClass('selected-drum');
     currentDrum = $(this).data('drum');
     $(this).addClass('selected-drum');
     setStepButtonState(currentDrum);
     $('.stepbutton[data-step='+(step-1)+']').toggleClass('stepOn');
-})
-$('.drum-click').on('mousedown', function(){
+  })
+  $('.drum-click').on('mousedown', function(){
     buttonDown.currentTime = 0;
     buttonDown.play();
-});
+  });
 
-$('.click').on('mousedown', function(){
+  $('.click').on('mousedown', function(){
     buttonDown.currentTime = 0;
     buttonDown.play();
-});
-$('.click').on('mouseup',function(){
+  });
+  $('.click').on('mouseup',function(){
     buttonUp.currentTime = 0;
     buttonUp.play();
-});
+  });
 
-$('.bpmfield').on('change', function(){
+  $('.bpmfield').on('change', function(){
     var number = $(this).val();
     var max = parseInt($(this).attr('max'));
     var min = parseInt($(this).attr('min'));
     console.log(number);
     //reset value to min or max if value exceeds either
     if(number > max){
-        $(this).val(max);
-        number = max;
+      $(this).val(max);
+      number = max;
     }else if(number < min){
-        $(this).val(min);
-        number = min;
+      $(this).val(min);
+      number = min;
     }
     bpm = number*4;
     beat = 60/bpm;
     console.log("bpm: "+bpm/4);
-});
+  });
 
-$('.bpmfield').on('keypress',function(event){
+  $('.bpmfield').on('keypress',function(event){
     if(event.which != 8 && event.which != 13){
-        if(event.which < 48 || event.which > 57){
-            event.preventDefault();
-        }
+      if(event.which < 48 || event.which > 57){
+        event.preventDefault();
+      }
     }
-});
+  });
 
-$(document).ready(function(){
-    setStepButtonState(currentDrum);
-    $('.bpmfield').val(bpm/4);
+
+
+  setStepButtonState(currentDrum);
+  $('.bpmfield').val(bpm/4);
 });
